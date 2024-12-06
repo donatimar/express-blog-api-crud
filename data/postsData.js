4130ee40ec7c27c3a2744f1ac24464b7f943fd4f
donatimar <donatimar@gmail.com>
2024-12-06 17:30:03 +0100

    modified array

--- data/postsData.js
+++ data/postsData.js
@@ -20,20 +20,6 @@ const posts = [
     image: "",
     category: ["tag3"],
   },
-  {
-    id: 4,
-    title: "Il mio quarto post",
-    content: "Questo è il content del quarto post",
-    image: "",
-    category: ["tag4"],
-  },
-  {
-    id: 5,
-    title: "Il mio quinto post",
-    content: "Questo è il content del quinto post",
-    image: "",
-    category: ["tag5"],
-  },
 ];
 
 module.exports = posts;
